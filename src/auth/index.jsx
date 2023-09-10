39a4105b75ac35dd98e219a572769ade637843ac
jusu <erikvixx@gmail.com>
2023-09-10 16:44:26 -0300

    deploy

--- src/auth/index.jsx
+++ src/auth/index.jsx
@@ -1,7 +1,7 @@
 export const CLIENT_ID = "13541e997e144450a6b9039c428b394d";
 export const CLIENT_SECRET = "8099ecccd74c41d4989f32341f29be69";
-export const AUTH_ENDPOINT = "http://localhost:5173/callback";
-export const URL_AFTER_LOGIN = "http://localhost:5173/home";
+export const AUTH_ENDPOINT = "https://chartfy.netlify.app/callback";
+export const URL_AFTER_LOGIN = "https://chartfy.netlify.app/home";
 export const SPACE_DELIMITER = "%20";
 export const SCOPES = [
     "user-read-private",
