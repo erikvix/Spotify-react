3314f987a423d5df2529352ccffff2b3defdacb5
Jusu <erikvixx@gmail.com>
2024-07-29 10:43:56 -0300

    refactor: samll changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import Login from "./pages/Login";
 import { storeToken } from "./api/storeToken";
 
 function App() {
-  const [url, setUrl] = useState(() => window.location.href);
   const token = localStorage.getItem("access_token");
   useEffect(() => {
-    if (url.includes("access_token")) {
-      storeToken();
-    }
-  }, [url]);
+    storeToken();
+  }, []);
 
   return (
     <div className="max-w-screen-xl mx-auto p-2">
