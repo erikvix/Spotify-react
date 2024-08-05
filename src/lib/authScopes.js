54a54fd319b422e3dcfec5b9fcf66ef52295cfc1
jusu <erikvixx@gmail.com>
2024-08-04 21:11:34 -0300

    refactor: updated scopes

--- src/lib/authScopes.js
+++ src/lib/authScopes.js
@@ -3,8 +3,6 @@ const scopes = [
   "user-read-private",
   "user-read-email",
   "user-top-read",
-  "user-read-recently-played",
-  "user-read-playback-position",
 ];
 
 export const scopesUrlParams = scopes.join(spaceDelimiter);
