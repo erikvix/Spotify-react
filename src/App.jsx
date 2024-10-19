21d9fea7c1ee7b8693977d084d973c791c59fbfe
jusu <erikvixx@gmail.com>
2024-10-18 23:22:26 -0300

    feat: adicionar direitos autorais e links para redes sociais no app
    Closes #4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { storeToken } from "./api/storeToken";
 import { Route, Router, Routes } from "react-router";
+import Footer from "./components/footer";
 
 function App() {
   useEffect(() => {
@@ -10,11 +11,14 @@ function App() {
   }, []);
 
   return (
-    <div className="max-w-screen-xl mx-auto p-2">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+    <div className="flex flex-col min-h-screen">
+      <div className="max-w-screen-xl mx-auto min-h-screen p-2">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
